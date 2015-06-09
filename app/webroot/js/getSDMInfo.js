b3fd5ae069e358468a3adc4e120f46a0a28c761e
lucas.lmendonca <lucas.lmendonca@dataprev.gov.br>
2015-06-09 14:01:13 -0300

    Resgate das informações do SDM nas RDMs

---
+++ app/webroot/js/getSDMInfo.js
@@ -0,0 +1,33 @@
+//Resgata informações de Demandas Cadastradas no Clarity
+function getSDMInfo(sdmid, control){
+  $.getJSON( "http://localhost:2380//wsdl-sdm/sgdGetSDMdoSelect.php?numero=" + sdmid, function( data ) {
+//  $.getJSON( "http://www-apps/_projects/dite/wsdl/sgdGetClarity.php?dmClarity=" + dm, function( data ) {
+    $.each( data, function( key, val ) {
+      if(key == 'dt_prevista')
+      {
+        $('ul#rdm').append($('ul#rdm').val() + "<li>" + "Data Prevista: " + val +"</li>");
+      }
+      if(key == 'dt_executada')
+      {
+        $('ul#rdm').append($('ul#rdm').val() + "<li>" + "Data de Execução: " + val +"</li>");
+      }
+      if(key == 'ambiente')
+      {
+        $('ul#rdm').append($('ul#rdm').val() + "<option value="+ val +" selected='selected'>Produção</option>");
+      }
+      $('#' + control + toCamelCase(key)).val(val);
+      $('.' + control + toCamelCase(key)).val(val);
+
+      $('#' + control + toCamelCase(key)).css('border', '1px solid #4cae4c');
+      $('.' + control + toCamelCase(key)).css('border', '1px solid #4cae4c');
+      //console.log('#' + control + toCamelCase(key) + " - " + val);
+    });
+  });
+  alert("Este processo pode demorar. Espere alguns segundos pela resposta do SDM!");
+}
+
+function toCamelCase(s) {
+  return (s||'').toLowerCase().replace(/(\b|_)\w/g, function(m) {
+    return m.toUpperCase().replace(/_/,'');
+  });
+}
